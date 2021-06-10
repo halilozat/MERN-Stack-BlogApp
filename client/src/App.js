138f7c0857f67bdbe1d89f5debb3637c4b12868f
halilozat <halilozat@hotmail.com>
2021-06-10 11:30:21 +0300

    bootstrap theme added & Header and Footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import CreateScreen from './Screens/CreateScreen';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Merhaba Dünya</h1>
-    </div>
+    <Router >
+      <Header />
+      <Container >
+        <Route path='/' component={HomeScreen} exact />
+        <Route path='/create' component={CreateScreen} />
+      </Container>
+      <Footer />
+    </Router>
+
   );
 }
 
